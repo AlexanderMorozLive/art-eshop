e4d38c6f8d8c1be7b3a00d4432d52ad8bf3b9c35
Alexander Moroz <iggwakeup@gmail.com>
2022-08-14 23:13:05 +0300

    find same article

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const siteArt = [[12345, 199],[13345, 199],[32345, 199],[12345, 1991]]
+const siteArt = [[12345, 199],[13345, 199],[32345, 199],[123457, 1991]]
 const warhouseArt = ['12345-1f1w', '12345-2342','32345~ffs', '12345', '32345']
 
 let warhouseTemp = []
 
 
-for (let i = 0; i <= warhouseArt.length; i++) {
-    // .
+for (let i = 0; i < warhouseArt.length; i++) {
+    let re = /(^[0-9]+)(.*)/
+    let str = `${warhouseArt[i]}`
+    let regExp = str.replace(re, '$1')
+
+    warhouseTemp.push([+regExp, warhouseArt[i]])
 }
+
+console.log(warhouseTemp)
 
 let temp = []
 
-for (let i = 0; i <= warhouseArt.length; i++) {
+for (let i = 0; i < warhouseTemp.length; i++) {
     for (let j = 0; j < siteArt.length; j++) {
-        if (+warhouseArt[i] === siteArt[j][0]) {
+        if (warhouseTemp[i][0] === siteArt[j][0]) {
             console.log(warhouseArt[i])
             temp.push([warhouseArt[i], siteArt[j][1]])
         }
